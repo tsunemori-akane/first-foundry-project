2b269d297de4d010c35b7ec80d6836a24373bf3e
cal <po6271933@outlook.com>
2024-11-12 00:03:19 +0800

    erc20 implement

--- test/mocks/MockERC20.sol
+++ test/mocks/MockERC20.sol
@@ -4,7 +4,7 @@ pragma solidity ^0.8.25;
 import { ERC20 } from "../../src/contracts/ERC20.sol";
 
 contract MockERC20 is ERC20 {
-    constructor(string memory name_, string memory symbol_, uint8 decimals_) ERC20(name_, symbol_, decimals_) { }
+    constructor(string memory name_, string memory symbol_, uint8 decimals_) ERC20("MockErc20", "MMMM", 18) { }
 
     function mint(address recipient_, uint256 amount_) external {
         return _mint(recipient_, amount_);
