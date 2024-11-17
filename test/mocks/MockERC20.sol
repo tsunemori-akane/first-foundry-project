bda54af38c025dfd5ce5db848a3e44b9092357a7
cal <po6271933@outlook.com>
2024-11-17 15:04:22 +0800

    mockErc20

--- test/mocks/MockERC20.sol
+++ test/mocks/MockERC20.sol
@@ -4,13 +4,17 @@ pragma solidity ^0.8.25;
 import { ERC20 } from "../../src/contracts/openzeppelin/ERC20.sol";
 
 contract MockERC20 is ERC20 {
-    constructor(string memory name_, string memory symbol_, uint8 decimals_) ERC20("MockErc20", "MMMM") { }
+    uint8 private _decimals;
+
+    constructor(string memory name_, string memory symbol_, uint8 decimals_) ERC20(name_, symbol_) {
+        _decimals = decimals_;
+    }
+
+    function decimals() public view override returns (uint8) {
+        return _decimals;
+    }
 
     function mint(address recipient_, uint256 amount_) external {
         return _mint(recipient_, amount_);
-    }
-
-    function burn(address owner_, uint256 amount_) external {
-        _burn(owner_, amount_);
     }
 }
